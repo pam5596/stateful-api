e1f645287c4d52459e85431c73c51eb7939e6925
pam5596 <ryosuke3191@gmail.com>
2026-07-25 13:14:10 +0900

    #2 middlewareを適応

--- src/applications/mhgm/index.ts
+++ src/applications/mhgm/index.ts
@@ -1,7 +1,8 @@
-import { BaseError } from "@abstructs";
 import { Hono } from "hono";
+import { useMiddlewares } from "./middlewares";
 
 const mhgm = new Hono().basePath("/mhgm")
+useMiddlewares(mhgm)
 
 mhgm.get('/', (c) => {
   return c.text("MHGM")
