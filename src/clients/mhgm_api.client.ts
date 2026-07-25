f9b383cc41385243f65d98a71c7cd2ef458458bd
pam5596 <ryosuke3191@gmail.com>
2026-07-26 03:52:26 +0900

    #3 MHGMAPIClientを実装

---
+++ src/clients/mhgm_api.client.ts
@@ -0,0 +1,59 @@
+import { callAPIHandler } from "@utils"
+import axios, { type AxiosInstance } from "axios"
+
+export class MHGMAPIClient {
+  private axios: AxiosInstance
+
+  constructor(
+    private api_key: string,
+    private base_url: string
+  ) {
+    this.axios = axios.create({
+      baseURL: base_url,
+      headers: { "X-API-KEY": api_key }
+    })
+  }
+
+  async put_user(req: { 
+    channel_id: string,
+    name: string,
+    avatar: string
+  }) {
+    return callAPIHandler(async () => {
+      return await this.axios.put<{ 
+        id: number 
+      }>(
+        "/users",
+        req
+      )
+    })
+  }
+
+  async get_user_keywords(req: {
+    user_id: number
+  }) {
+    return await this.axios.get<{ 
+      keywords: {
+        id: number,
+        keyword: string,
+        action: string
+      }[]
+    }>(
+      `/users/${req.user_id}/keywords`
+    )
+  }
+
+  async post_action_log(req: {
+    message: string,
+    user_id: number,
+    broadcast_id: number,
+    keyword_id: number
+  }) {
+    return await this.axios.post<{
+      id: number
+    }>(
+      "/action-logs",
+      req
+    )
+  }
+}
