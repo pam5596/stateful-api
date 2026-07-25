7d430731b774f242c07aa1b10d9999c5b8fec665
pam5596 <ryosuke3191@gmail.com>
2026-07-25 22:29:31 +0900

    #3 ForeignAPICallErrorとUnknownErrorを実装

--- src/errors/unknown.error.ts
+++ src/errors/unknown.error.ts
@@ -1,11 +1,12 @@
 import { BaseError } from "@abstructs";
 
 export class UnknownError extends BaseError {
-  constructor(catched: unknown) {
+  constructor(catched: unknown, instance?: string) {
     super(
       500,
       "base.errors.unknown",
       String(catched),
+      instance
     )
   }
 }
