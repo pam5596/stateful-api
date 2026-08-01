b0e9471cc60dbac085614e1c9fcf341a6aab09c1
pam5596 <ryosuke3191@gmail.com>
2026-08-01 22:28:51 +0900

    #4 socket_io_member_routeを実装

---
+++ src/applications/mhgm/routes/socket.io_member.route.ts
@@ -0,0 +1,11 @@
+import { socketIOClient } from "@instances";
+
+export const socket_io_member_route = async () => {
+  socketIOClient.of("/member").on("connection", async (socket) => {
+    console.log('[Socket.io]Client connected:', socket.id)
+
+    socket.on("disconnect", () => {
+      console.log('[Socket.io]Client disconnected:', socket.id)
+    })
+  })
+}
