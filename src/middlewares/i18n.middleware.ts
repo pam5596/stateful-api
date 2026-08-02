ae2624ca715c101091e00f8fa1fac3dda90dcfb9
pam5596 <ryosuke3191@gmail.com>
2026-08-02 16:54:03 +0900

    #9 i18nミドルウェアにデフォルトロケールを実装

--- src/middlewares/i18n.middleware.ts
+++ src/middlewares/i18n.middleware.ts
@@ -1,9 +1,15 @@
-import { defineIntlifyMiddleware, detectLocaleFromAcceptLanguageHeader } from "@intlify/hono"
+import { defineIntlifyMiddleware, detectLocaleFromAcceptLanguageHeader, getQueryLocale } from "@intlify/hono"
 
 import { ja } from "@locales"
 
 export const i18nMiddleware = defineIntlifyMiddleware<[typeof ja],'ja'>({
-  locale: detectLocaleFromAcceptLanguageHeader,
+  locale: (ctx: Request): string => {
+    try {
+      return getQueryLocale(ctx).toString()
+    } catch {
+      return process.env.DEFAULT_LOCALE!
+    }
+  },
   messages: {
     ja
   }
