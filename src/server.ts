edd1bed180b78e907b531dabd62dc7ba51c39c8b
pam5596 <ryosuke3191@gmail.com>
2026-08-02 17:00:57 +0900

    #9 health-checkルートを実装

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,20 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import 'dotenv/config'
 
 import applications from './applications'
 import { useMiddlewares } from "./middlewares"
 import { socketIOClient } from "@instances"
+import { defineRoutes } from './routes'
 
 const app = new Hono()
+
 useMiddlewares(app)
+defineRoutes(app)
 
 app.route("/", applications)
 
-export const server = serve({
+const server = serve({
   fetch: app.fetch,
   port: 5000
 }, (info) => {
