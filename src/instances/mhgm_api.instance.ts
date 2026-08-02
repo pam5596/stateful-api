d273c27fad21e13c61917f76ffa59a948398ed72
pam5596 <ryosuke3191@gmail.com>
2026-08-02 16:58:27 +0900

    #9 BASE_URLの環境変数名を変更

--- src/instances/mhgm_api.instance.ts
+++ src/instances/mhgm_api.instance.ts
@@ -1,6 +1,6 @@
 import { MHGMAPIClient } from "@clients";
 
 export const mhgmAPIClient = new MHGMAPIClient(
-  process.env.MHGM_PRIVATE_API_KEY!,
-  process.env.MHGM_PRIVATE_BASE_URL!
+  process.env.MHGM_API_KEY!,
+  process.env.MHGM_SERVER_URL!
 )
