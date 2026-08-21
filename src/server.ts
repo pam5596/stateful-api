935c4ac17bb234b1e7f799d655310531a65601ed
pam5596 <ryosuke3191@gmail.com>
2026-08-22 02:03:41 +0900

    #9 ポートを5000->8000に変更

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ app.route("/", applications)
 
 const server = serve({
   fetch: app.fetch,
-  port: 5000
+  port: 8000
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
