f2648df563d73cb8d441baa9d286a9639cb2a0dd
pam5596 <ryosuke3191@gmail.com>
2026-08-22 19:02:03 +0900

    #1 health-checkルートをapikey middlewareのチェック範囲から除外

--- src/middlewares/apiKeyAuth.middleware.ts
+++ src/middlewares/apiKeyAuth.middleware.ts
@@ -3,6 +3,9 @@ import type { MiddlewareHandler } from "hono"
 
 export const apiKeyAuthMiddleware: MiddlewareHandler = async (c, next) => {
   const api_key = c.req.header("x-api-key")
-  if (api_key !== process.env.API_KEY) throw new InvalidAPIKeyError()
+  if (
+    api_key !== process.env.API_KEY &&
+    c.req.path !== '/health-check'
+  ) throw new InvalidAPIKeyError()
   await next()
 }
